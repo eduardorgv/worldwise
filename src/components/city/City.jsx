274e9465d0ce2c7dd81065b22780468c7c46a1f5
eduardorgv <eduardsabb@outlook.com>
2023-08-23 13:27:16 -0600

    Fetching city

--- src/components/city/City.jsx
+++ src/components/city/City.jsx
@@ -1,32 +1,57 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useCities } from "../../contexts/CitiesContext";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import styles from './City.module.css'
+import { useFormatDate } from "../../hooks/useFomatDate";
+import { Spinner } from "../spinner/Spinner";
+import { BackButton } from "../button/BackButton";
 
 export const City = () => {
   const { id } = useParams();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
-
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
-
+  const { getCity, currentCity, isLoading } = useCities();
   const { cityName, emoji, date, notes } = currentCity;
 
+  useEffect(() => {
+    getCity(id)
+  }, [id])
+  
+  if(isLoading) return <Spinner />
+
   return (
-    <div className={styles.mapContainer}>
-      <h1>City {id}</h1>
-      <h1>
-        Position: {lat}, {lng}
-      </h1>
-      <button
-        onClick={() => setSearchParams({ lat: 23, lng: 50 })}
-      >
-        Change position
-      </button>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{useFormatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        <BackButton />
+      </div>
     </div>
   );
 };
--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,28 +6,46 @@ const BASE_URL = "http://localhost:8000";
 export const CitiesProvider = ({ children }) => {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
-    const fetchCities = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch (error) {
-        console.error("Error ❌: ", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
     fetchCities();
   }, []);
+
+  const fetchCities = async () => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities`);
+      const data = await res.json();
+      setCities(data);
+    } catch (error) {
+      console.error("Error ❌: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const getCity = async (id) => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (error) {
+      console.error("Error ❌: ", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <CitiesContext.Provider
-        value={{
-            cities,
-            isLoading
-        }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
@@ -35,8 +53,10 @@ export const CitiesProvider = ({ children }) => {
 };
 
 export const useCities = () => {
-    const context = useContext(CitiesContext);
-    if(context === undefined)
-        throw new Error('Error ❌: CitiesContext was used outside the CitiesProvider');
-    return context;
-}
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error(
+      "Error ❌: CitiesContext was used outside the CitiesProvider"
+    );
+  return context;
+};

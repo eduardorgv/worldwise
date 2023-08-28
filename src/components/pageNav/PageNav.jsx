ae079aab8bdef4caad79ea2896ddb3ddf106f360
eduardorgv <eduardsabb@outlook.com>
2023-08-28 09:07:28 -0600

    Landing page branch

--- src/components/pageNav/PageNav.jsx
+++ src/components/pageNav/PageNav.jsx
@@ -9,7 +9,7 @@ export const PageNav = () => {
         <ul>
             <li><NavLink to='/pricing'>Pricing</NavLink></li>
             <li><NavLink to='/product'>Product</NavLink></li>
-            <li><NavLink to='/login' className={styles.ctaLink}>Login</NavLink></li>
+            {/* <li><NavLink to='/login' className={styles.ctaLink}>Login</NavLink></li> */}
         </ul>
     </nav>
   )
